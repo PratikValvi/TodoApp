d6363ff29f9013f677a6fc355ab85bb4115d0de7
Pratik Rajendra Valvi <pratikvalvi2305@gmail.com>
2021-07-08 17:49:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Provider } from 'react-redux';
+import { Container } from 'semantic-ui-react';
+import Header from './Components/Header';
+import InputField from './Components/InputField';
+import store from './store/store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Provider store={store}>
+      <Container>
+        <Header/>
+        <InputField/>
+      </Container>
+    </Provider>
+    </>
   );
 }
 
