1144ad7e5199748751490def17d30a5216c86241
Pratik Valvi <pratikvalvi2305@gmail.com>
2021-07-09 10:22:51 +0530

    Added Header,Inputfield Components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Provider } from 'react-redux';
 import { Container } from 'semantic-ui-react';
 import Header from './Components/Header';
 import InputField from './Components/InputField';
-import store from './store/store';
+// import store from './store/store';
 
 function App() {
   return (
     <>
-    <Provider store={store}>
       <Container>
         <Header/>
         <InputField/>
       </Container>
-    </Provider>
     </>
   );
 }
